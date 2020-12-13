64e17542398ba120b1edeeb2395cefd1033a85e1
Richard Blažek <blazek.risa@gmail.com>
2020-12-13 13:25:03 +0100

    Docs updated

--- ImperitWASM/Client/ListExtensions.cs
+++ ImperitWASM/Client/ListExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace ImperitWASM.Client
 {
-	public static class ClientExtensions
+	public static class ListExtensions
 	{
 		public static IEnumerable<T> ReverseIf<T>(this IEnumerable<T> e, bool reverse) => reverse ? e.Reverse() : e;
 		public static IEnumerable<(int i, T v)> Index<T>(this IEnumerable<T> e) => e.Select((v, i) => (i, v));

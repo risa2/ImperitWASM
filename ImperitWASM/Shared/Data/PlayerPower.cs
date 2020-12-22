221f3e7bf26f7b44dd7d361b045818d2fd74ab5b
Richard Blažek <blazek.risa@gmail.com>
2020-12-22 15:01:28 +0100

    UI minor improvements, 3/4 final countries instead of 4/5

--- ImperitWASM/Shared/Data/PlayerPower.cs
+++ ImperitWASM/Shared/Data/PlayerPower.cs
@@ -2,6 +2,6 @@ namespace ImperitWASM.Shared.Data
 {
 	public record PlayerPower(bool Alive, int Final, int Income, int Lands, int Money, int Soldiers)
 	{
-		public int Total => Alive ? Soldiers + Money + (Income * 5) + (Final * 50) : 0;
+		public int Total => Alive ? Soldiers + Money + (Income * 5) + (Final * 100) : 0;
 	}
 }

290ec459434325112a0b0afa4473a0750cca0a00
Richard Blažek <blazek.risa@gmail.com>
2021-03-11 17:36:19 +0100

    More bug fixes

--- ImperitWASM/Shared/Data/Game.cs
+++ ImperitWASM/Shared/Data/Game.cs
@@ -7,7 +7,7 @@ namespace ImperitWASM.Shared.Data
 		public enum State { Created, Countdown, Started, Finished, Invalid = -1 }
 		public static Game Create => new Game(State.Created, DateTimeOffset.MaxValue, DateTimeOffset.MaxValue);
 		public static Game CountDown(TimeSpan delay) => new Game(State.Countdown, DateTimeOffset.UtcNow.Add(delay), DateTimeOffset.MaxValue);
-		public static Game Start => new Game(State.Countdown, DateTimeOffset.UtcNow, DateTimeOffset.MaxValue);
+		public static Game Start => new Game(State.Started, DateTimeOffset.UtcNow, DateTimeOffset.MaxValue);
 		public Game Finish() => this with { Current = State.Finished, FinishTime = DateTimeOffset.UtcNow };
 
 		public bool ShouldStart => Current == State.Countdown && DateTimeOffset.UtcNow >= StartTime;

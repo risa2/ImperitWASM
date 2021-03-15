43262b5968ff257083646ee7499b93c5f25bde63
Richard Blažek <blazek.risa@gmail.com>
2021-03-15 12:15:59 +0100

    Bug fixes

--- ImperitWASM/Client/Services/SessionService.cs
+++ ImperitWASM/Client/Services/SessionService.cs
@@ -9,12 +9,19 @@ namespace ImperitWASM.Client.Services
 		public Data.Session Session { get; private set; } = new Data.Session();
 		public string? Name { get; private set; }
 		public SessionService(ISessionStorageService iss) => this.iss = iss;
-		public async Task LoadAsync() => Session = await iss.GetItemAsync<Data.Session?>("session") ?? new Data.Session();
-		public Task SetAsync(Data.Session? session, string? name = null)
+		public async Task LoadAsync()
+		{
+			Session = await iss.GetItemAsync<Data.Session?>("session") ?? new Data.Session();
+			Name = await iss.GetItemAsync<string?>("name");
+		}
+
+		Task SetItemAsync<T>(string key, T? value) => value is null ? iss.RemoveItemAsync(key) : iss.SetItemAsync(key, value);
+		public async Task SetAsync(Data.Session? session, string? name = null)
 		{
 			Name = name;
 			Session = session ?? new Data.Session();
-			return !IsSet ? iss.RemoveItemAsync("session") : iss.SetItemAsync("session", Session);
+			await SetItemAsync("session", IsSet ? Session : null);
+			await SetItemAsync("name", Name);
 		}
 		public string Key => Session.Key;
 		public int GameId => Session.G;
